972eb358b9d9094b1a3fc0c06a12832cf53915f6
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-24 12:10:11 -0600

    Add timers with ref

--- src/components/TimerChallenge/TimerChallenge.jsx
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react'
 import './TimerChallenge.css'
 
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const timer = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerRunning(false);
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerRunning(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return <section className="challenge">
     <h2>{title}</h2>
     <p>{targetTime} second{targetTime > 1 && "s"}</p>
-    <button>Start Challenge</button>
-    <p>Time is runnin... / Timer inactive</p>
+    <button onClick={timerRunning ? handleStop : handleStart}>
+      {timerRunning ? "Stop Timer" : "Start Challenge"}
+    </button>
+    <p className={`${timerRunning && "active"}`}>
+      {timerRunning ? "Timer is runnin..." : "Timer inactive"}
+    </p>
   </section>
 }
